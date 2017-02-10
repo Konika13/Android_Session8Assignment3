1564760472be064345d53859424898fb46c4e3f3
Konika13 <konika.agarwal1994@gmail.com>
2017-02-10 19:05:09 +0530

    Add files via upload

---
+++ script8.3.js
@@ -0,0 +1,12 @@
+function hidefunc(){
+        $("#myid").hide();
+    }
+    function hidefunc1(){
+        $(".myclass").hide();
+    }
+    function showfunc(){
+        $('#myid').show();
+    }
+    function showfunc1(){
+        $('.myclass').show();
+    }
